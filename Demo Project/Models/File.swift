1e09c4aa0d1372f09091aa4e7cabac443880f5e7
Dheeraj Bhavsar <dheerajbhavsar940@gmail.com>
2018-10-14 13:04:02 +0530

    commit before pull

--- Demo Project/Models/File.swift
+++ Demo Project/Models/File.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class File: Codable {
     
-    case fileName
-    case fileType
-    //        case fileData             Skips file data in json encoding
-    //        case fileThumbnail
+    
     
     var fileName: String?
     var fileData: Data?
